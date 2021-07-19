4534ab6bf578822f43c7bd55cf3e37840d39f823
Guillaumo <wangtseming@gmail.com>
2021-07-19 10:08:32 +0200

    sécurisation de l'interface admin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -15,7 +15,7 @@ class DashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
-        return parent::index();
+        return $this->render('admin/index.html.twig');
     }
 
     public function configureDashboard(): Dashboard
